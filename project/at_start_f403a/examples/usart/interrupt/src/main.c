fb56ed6b271801655abec766f7195129cf5a7517
YI-CHENG <yich_liao@mail.davicom.com.tw>
2023-12-18 19:10:30 +0800

    printf test ok

--- project/at_start_f403a/examples/usart/interrupt/src/main.c
+++ project/at_start_f403a/examples/usart/interrupt/src/main.c
@@ -1,41 +1,41 @@
 /**
-  **************************************************************************
-  * @file     main.c
-  * @brief    main program
-  **************************************************************************
-  *                       Copyright notice & Disclaimer
-  *
-  * The software Board Support Package (BSP) that is made available to
-  * download from Artery official website is the copyrighted work of Artery.
-  * Artery authorizes customers to use, copy, and distribute the BSP
-  * software and its related documentation for the purpose of design and
-  * development in conjunction with Artery microcontrollers. Use of the
-  * software is governed by this copyright notice and the following disclaimer.
-  *
-  * THIS SOFTWARE IS PROVIDED ON "AS IS" BASIS WITHOUT WARRANTIES,
-  * GUARANTEES OR REPRESENTATIONS OF ANY KIND. ARTERY EXPRESSLY DISCLAIMS,
-  * TO THE FULLEST EXTENT PERMITTED BY LAW, ALL EXPRESS, IMPLIED OR
-  * STATUTORY OR OTHER WARRANTIES, GUARANTEES OR REPRESENTATIONS,
-  * INCLUDING BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY,
-  * FITNESS FOR A PARTICULAR PURPOSE, OR NON-INFRINGEMENT.
-  *
-  **************************************************************************
-  */
+ **************************************************************************
+ * @file     main.c
+ * @brief    main program
+ **************************************************************************
+ *                       Copyright notice & Disclaimer
+ *
+ * The software Board Support Package (BSP) that is made available to
+ * download from Artery official website is the copyrighted work of Artery.
+ * Artery authorizes customers to use, copy, and distribute the BSP
+ * software and its related documentation for the purpose of design and
+ * development in conjunction with Artery microcontrollers. Use of the
+ * software is governed by this copyright notice and the following disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED ON "AS IS" BASIS WITHOUT WARRANTIES,
+ * GUARANTEES OR REPRESENTATIONS OF ANY KIND. ARTERY EXPRESSLY DISCLAIMS,
+ * TO THE FULLEST EXTENT PERMITTED BY LAW, ALL EXPRESS, IMPLIED OR
+ * STATUTORY OR OTHER WARRANTIES, GUARANTEES OR REPRESENTATIONS,
+ * INCLUDING BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE, OR NON-INFRINGEMENT.
+ *
+ **************************************************************************
+ */
 
 #include "at32f403a_407_board.h"
 #include "at32f403a_407_clock.h"
 
 /** @addtogroup AT32F403_periph_examples
-  * @{
-  */
+ * @{
+ */
 
 /** @addtogroup 403_USART_interrupt USART_interrupt
-  * @{
-  */
+ * @{
+ */
 
-#define COUNTOF(a)                       (sizeof(a) / sizeof(*(a)))
-#define USART2_TX_BUFFER_SIZE            (COUNTOF(usart2_tx_buffer) - 1)
-#define USART3_TX_BUFFER_SIZE            (COUNTOF(usart3_tx_buffer) - 1)
+#define COUNTOF(a) (sizeof(a) / sizeof(*(a)))
+#define USART2_TX_BUFFER_SIZE (COUNTOF(usart2_tx_buffer) - 1)
+#define USART3_TX_BUFFER_SIZE (COUNTOF(usart3_tx_buffer) - 1)
 
 uint8_t usart2_tx_buffer[] = "usart transfer by interrupt: usart2 -> usart3 using interrupt";
 uint8_t usart3_tx_buffer[] = "usart transfer by interrupt: usart3 -> usart2 using interrupt";
@@ -49,10 +49,10 @@ uint8_t usart2_tx_buffer_size = USART2_TX_BUFFER_SIZE;
 uint8_t usart3_tx_buffer_size = USART3_TX_BUFFER_SIZE;
 
 /**
-  * @brief  config usart
-  * @param  none
-  * @retval none
-  */
+ * @brief  config usart
+ * @param  none
+ * @retval none
+ */
 void usart_configuration(void)
 {
   gpio_init_type gpio_init_struct;
@@ -69,7 +69,7 @@ void usart_configuration(void)
 
   /* configure the usart2 tx pin */
   gpio_init_struct.gpio_drive_strength = GPIO_DRIVE_STRENGTH_STRONGER;
-  gpio_init_struct.gpio_out_type  = GPIO_OUTPUT_PUSH_PULL;
+  gpio_init_struct.gpio_out_type = GPIO_OUTPUT_PUSH_PULL;
   gpio_init_struct.gpio_mode = GPIO_MODE_MUX;
   gpio_init_struct.gpio_pins = GPIO_PINS_2;
   gpio_init_struct.gpio_pull = GPIO_PULL_NONE;
@@ -81,7 +81,7 @@ void usart_configuration(void)
 
   /* configure the usart2 rx pin */
   gpio_init_struct.gpio_drive_strength = GPIO_DRIVE_STRENGTH_STRONGER;
-  gpio_init_struct.gpio_out_type  = GPIO_OUTPUT_PUSH_PULL;
+  gpio_init_struct.gpio_out_type = GPIO_OUTPUT_PUSH_PULL;
   gpio_init_struct.gpio_mode = GPIO_MODE_INPUT;
   gpio_init_struct.gpio_pins = GPIO_PINS_3;
   gpio_init_struct.gpio_pull = GPIO_PULL_UP;
@@ -118,17 +118,17 @@ void usart_configuration(void)
 }
 
 /**
-  * @brief  compares two buffers.
-  * @param  pbuffer1, pbuffer2: buffers to be compared.
-  * @param  buffer_length: buffer's length
-  * @retval 1: pbuffer1 identical to pbuffer2
-  *         0: pbuffer1 differs from pbuffer2
-  */
-uint8_t buffer_compare(uint8_t* pbuffer1, uint8_t* pbuffer2, uint16_t buffer_length)
+ * @brief  compares two buffers.
+ * @param  pbuffer1, pbuffer2: buffers to be compared.
+ * @param  buffer_length: buffer's length
+ * @retval 1: pbuffer1 identical to pbuffer2
+ *         0: pbuffer1 differs from pbuffer2
+ */
+uint8_t buffer_compare(uint8_t *pbuffer1, uint8_t *pbuffer2, uint16_t buffer_length)
 {
-  while(buffer_length--)
+  while (buffer_length--)
   {
-    if(*pbuffer1 != *pbuffer2)
+    if (*pbuffer1 != *pbuffer2)
     {
       return 0;
     }
@@ -139,41 +139,48 @@ uint8_t buffer_compare(uint8_t* pbuffer1, uint8_t* pbuffer2, uint16_t buffer_len
 }
 
 /**
-  * @brief  main function.
-  * @param  none
-  * @retval none
-  */
+ * @brief  main function.
+ * @param  none
+ * @retval none
+ */
 int main(void)
 {
   system_clock_config();
   at32_board_init();
+  uart_print_init(115200);
+  printf(": test start...\r\n");
   usart_configuration();
-  at32_led_toggle(LED2);    
-  /* wait until end of transmission from usart2 to usart3 */
-  while(usart3_rx_counter < usart2_tx_buffer_size);
-  at32_led_toggle(LED3);
-  /* wait until end of transmission from usart3 to usart2 */
-  while(usart2_rx_counter < usart3_tx_buffer_size);
-  at32_led_toggle(LED4);
-  while(1)
-  {
 
+  at32_led_toggle(LED2);
+  /* wait until end of transmission from usart2 to usart3 */
+  while (usart3_rx_counter < usart2_tx_buffer_size)
+    ;
+  at32_led_toggle(LED3);
+  printf(": at32_led_toggle(LED3)...\r\n");
+  /* wait until end of transmission from usart3 to usart2 */
+  while (usart2_rx_counter < usart3_tx_buffer_size)
+    ;
+  at32_led_toggle(LED4);
+  printf(": at32_led_toggle(LED4)...\r\n");
+  while (1)
+  {
     /* compare data buffer */
-    if(buffer_compare(usart2_tx_buffer, usart3_rx_buffer, USART2_TX_BUFFER_SIZE) && \
-       buffer_compare(usart3_tx_buffer, usart2_rx_buffer, USART3_TX_BUFFER_SIZE))
+    if (buffer_compare(usart2_tx_buffer, usart3_rx_buffer, USART2_TX_BUFFER_SIZE) &&
+        buffer_compare(usart3_tx_buffer, usart2_rx_buffer, USART3_TX_BUFFER_SIZE))
     {
       at32_led_toggle(LED2);
       at32_led_toggle(LED3);
       at32_led_toggle(LED4);
       delay_sec(1);
+      printf(": at32_led_toggle(all)...\r\n");
     }
   }
 }
 
 /**
-  * @}
-  */
+ * @}
+ */
 
 /**
-  * @}
-  */
+ * @}
+ */
